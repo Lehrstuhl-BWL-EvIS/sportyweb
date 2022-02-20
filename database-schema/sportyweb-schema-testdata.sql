c26728d12aa87bfadd2e91ce5f1406d5d66e959f
Stefan Strecker <stefan.strecker@fernuni-hagen.de>
2022-02-20 11:47:07 +0100

    updated multiple files

--- database-schema/sportyweb-schema-testdata.sql
+++ database-schema/sportyweb-schema-testdata.sql
@@ -1,3 +1,20 @@
+/*  Copyright (C) 2021-2022 Stefan Strecker
+    https://gitlab.com/fuhevis/sportyweb
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published
+    by the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 /* --------------------------------------------- Testdaten für mitglieder */
 INSERT INTO mitglied VALUES 
 (DEFAULT, 'Mustermann', 'Max', '1970-01-01', 'm', 
