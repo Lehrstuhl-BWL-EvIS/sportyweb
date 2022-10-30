727cc438d26b3c3a3019a3ce40b6c13996bc7416
krewast <bastian.kres@krewast.de>
2022-10-30 11:59:52 +0100

    Seed: Add default user accounts (in the dev env) to make resetting the database on a regular basis painless.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,36 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias Sportyweb.Accounts
+
+###################################
+# Add user accounts.
+# Only in the dev environment!
+
+if Mix.env() in [:dev] do
+  Accounts.register_user(%{
+    email: "stefan.strecker@fernuni-hagen.de",
+    password: "NTU5MTM5NGNmZjY"
+  })
+
+  Accounts.register_user(%{
+    email: "marvin.biesenbach@studium.fernuni-hagen.de",
+    password: "MzU2OWY3NTQyNzI"
+  })
+
+  Accounts.register_user(%{
+    email: "sven.christ@fernuni-hagen.de",
+    password: "ZThjNWY2NTQ3OGQ"
+  })
+
+  Accounts.register_user(%{
+    email: "bastian.kres@krewast.de",
+    password: "MzU0MmJiZWI4ZmN"
+  })
+
+  Accounts.register_user(%{
+    email: "andrew.utley@studium.fernuni-hagen.de",
+    password: "MGI3MTNlMzczZjR"
+  })
+end
