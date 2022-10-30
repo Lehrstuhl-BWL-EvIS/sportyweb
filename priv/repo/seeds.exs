a7492f85fde1dcae65a4b3f8aaa84d6e12cea857
krewast <bastian.kres@krewast.de>
2022-10-30 15:35:24 +0100

    Seed: Added a few example clubs

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,10 +10,13 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
+alias Sportyweb.Repo
+
 alias Sportyweb.Accounts
+alias Sportyweb.Organizations.Club
 
 ###################################
-# Add user accounts.
+# Add Users
 # Only in the dev environment!
 
 if Mix.env() in [:dev] do
@@ -42,3 +45,34 @@ if Mix.env() in [:dev] do
     password: "MGI3MTNlMzczZjR"
   })
 end
+
+###################################
+# Add Clubs
+
+Repo.insert!(%Club{
+  name: "FC Bayern München",
+  reference_number: "0815",
+  website_url: "https://fcbayern.com/",
+  founding_date: ~D[1900-02-27]
+})
+
+Repo.insert!(%Club{
+  name: "1. FC Köln",
+  reference_number: "xyz",
+  website_url: "https://fc.de/",
+  founding_date: ~D[1948-02-13]
+})
+
+Repo.insert!(%Club{
+  name: "FC St. Pauli",
+  reference_number: "",
+  website_url: "https://www.fcstpauli.com/",
+  founding_date: ~D[1910-05-15]
+})
+
+Repo.insert!(%Club{
+  name: "Keine-Website-Verein",
+  reference_number: "",
+  website_url: "",
+  founding_date: ~D[2020-04-01]
+})
