b61e44bda426a47e23e559c0d95185bce786063d
Andrew <andrew.utley@studium.fernuni-hagen.de>
2022-12-03 17:04:32 +0100

    convenience udpates root and login

--- lib/sportyweb_web/live/user_login_live.ex
+++ lib/sportyweb_web/live/user_login_live.ex
@@ -41,38 +41,11 @@ defmodule SportywebWeb.UserLoginLive do
     </div>
     <div class="mx-auto max-w-sm mt-10">
       <.header class="text-center bg-red-600">DEV MODE Quick Sign In</.header>
-      <.simple_form
-        :let={f}
-        id="login_form"
-        for={:user}
-        action={~p"/users/log_in"}
-        as={:user}
-        phx-update="ignore"
-      >
-        <.input field={{f, :email}} value ="sportyweb_admin@tester.de" type="hidden" required />
-        <.input field={{f, :password}} value ="testertester" type="hidden" required />
-        <:actions>
-          <.button phx-disable-with="Sigining in..." class="w-full">
-            Sportyweb Admin Tester <span aria-hidden="true">→</span>
-          </.button>
-        </:actions>
-      </.simple_form>
-      <.simple_form
-        :let={f}
-        id="login_form"
-        for={:user}
-        action={~p"/users/log_in"}
-        as={:user}
-        phx-update="ignore"
-      >
-        <.input field={{f, :email}} value ="clubmember@tester.de" type="hidden" required />
-        <.input field={{f, :password}} value ="testertester" type="hidden" required />
-        <:actions>
-          <.button phx-disable-with="Sigining in..." class="w-full">
-            Member Tester <span aria-hidden="true">→</span>
-          </.button>
-        </:actions>
-      </.simple_form>
+      <.quicklog email="sportyweb_admin@tester.de"/>
+      <.quicklog email="clubadmin@tester.de"/>
+      <.quicklog email="clubsubadmin@tester.de"/>
+      <.quicklog email="clubreadwritemember@tester.de"/>
+      <.quicklog email="clubmember@tester.de"/>
     </div>
     """
   end
@@ -81,4 +54,31 @@ defmodule SportywebWeb.UserLoginLive do
     email = live_flash(socket.assigns.flash, :email)
     {:ok, assign(socket, email: email), temporary_assigns: [email: nil]}
   end
+
+
+  # Support quick login #For testing purposes only!!
+  use Phoenix.Component
+
+  attr :email, :string, default: "error"
+  def quicklog(assigns) do
+    ~H"""
+      <.simple_form
+      :let={f}
+      id="login_form"
+      for={:user}
+      action={~p"/users/log_in"}
+      as={:user}
+      phx-update="ignore"
+    >
+      <.input field={{f, :email}} value = {@email} type="hidden" required />
+      <.input field={{f, :password}} value ="testertester" type="hidden" required />
+      <:actions>
+        <.button phx-disable-with="Sigining in..." class="w-full">
+          <%= @email %><span aria-hidden="true">→</span>
+        </.button>
+      </:actions>
+    </.simple_form>
+    """
+  end
+
 end
