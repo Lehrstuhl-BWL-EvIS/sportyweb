6f1fbbae947c549337fc005dd071b434c1d3c9c5
krewast <bastian.kres@krewast.de>
2022-12-03 19:09:04 +0100

    Root page: Automatically edirect to the list of clubs if the user is logged in

--- lib/sportyweb_web/controllers/page_controller.ex
+++ lib/sportyweb_web/controllers/page_controller.ex
@@ -2,6 +2,11 @@ defmodule SportywebWeb.PageController do
   use SportywebWeb, :controller
 
   def home(conn, _params) do
+    if conn.assigns[:current_user] do
+      conn
+      |> redirect(to: ~p"/clubs")
+    end
+
     # The home page is often custom made,
     # so skip the default app layout.
     render(conn, :home, layout: false)
