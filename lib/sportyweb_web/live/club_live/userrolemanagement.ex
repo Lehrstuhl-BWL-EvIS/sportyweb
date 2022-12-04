be0abac3fbd9ee7daecfc177313fad9f9db2d0c0
Andrew <andrew.utley@studium.fernuni-hagen.de>
2022-12-04 21:01:19 +0100

    update to skeleton to update club roles of club members

--- lib/sportyweb_web/live/club_live/userrolemanagement.ex
+++ lib/sportyweb_web/live/club_live/userrolemanagement.ex
@@ -7,26 +7,26 @@ defmodule SportywebWeb.ClubLive.Userrolemanagement do
 
   @impl true
   def mount(%{"id" => club_id}, _session, socket) do
-    if PolicyClub.can?(socket.assigns.current_user, :userrolemanagement, %{"id" => club_id}) do
       {:ok,
         socket
         |> assign(:club, Organization.get_club!(club_id))
         |> assign(:clubmembers, PolicyClub.get_club_members_and_their_roles(club_id))
-        |> assign(:roles, ["club_subadmin", "club_member"])}
+        |> assign(:roles, ["club_subadmin", "club_readwrite_member", "club_member"])}
+  end
+
+  @impl true
+  def handle_params(%{"id" => club_id}, _, socket) do
+    if PolicyClub.can?(socket.assigns.current_user, socket.assigns.live_action, %{"id" => club_id}) do
+      {:noreply, socket}
     else
-      {:ok, socket
+      {:noreply, socket
         |> put_flash(:error, "No permission to manage user roles of club members.")
         |> redirect(to: ~p"/clubs/#{club_id}")}
     end
   end
 
   @impl true
-  def handle_params(%{"id" => id}, _, socket) do
-    {:noreply, socket}
-  end
-
-  @impl true
-  def handle_event("role_selected", %{"role" => newrole}, socket) do
+  def handle_event("role_selected", %{"role" => newrole}, socket) do #TODO
     {:noreply, socket}
   end
 end
