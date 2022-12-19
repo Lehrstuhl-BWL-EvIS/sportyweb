825b18b7a03f4bbf511defee1353f11eea02511e
krewast <bastian.kres@krewast.de>
2022-12-19 10:46:36 +0100

    Renamed "SidebarNavigationComponent" --> "ClubNavigationComponent". Created reusable helper function to determine whether to show this navigation based on multiple conditions like "Is the user logged in?" and "Is the user currently "inside" an existing club?".

--- lib/sportyweb_web/components/layouts.ex
+++ lib/sportyweb_web/components/layouts.ex
@@ -2,4 +2,14 @@ defmodule SportywebWeb.Layouts do
   use SportywebWeb, :html
 
   embed_templates "layouts/*"
+
+  def show_club_navigation(%{current_user: current_user, club: club}) do
+    # Only show the club sidebar for logged in users and "inside"
+    # existing clubs. All existing clubs have an ID.
+    current_user && club && club.id
+  end
+
+  def show_club_navigation(_assigns) do
+    false
+  end
 end
