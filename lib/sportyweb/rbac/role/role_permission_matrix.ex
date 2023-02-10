f96f091ea1e0e586dac5856b5c48a7173280c099
Andrew <andrew.utley@studium.fernuni-hagen.de>
2023-02-10 16:44:07 +0100

    Role permission matrix created

---
+++ lib/sportyweb/rbac/role/role_permission_matrix.ex
@@ -0,0 +1,39 @@
+defmodule Sportyweb.RBAC.Role.RolePermissionMatrix do
+
+  def role_permission_matrix(:application) do
+    [
+      sportyweb_administrator: []
+    ]
+  end
+
+  def role_permission_matrix(:club) do
+    [
+      vereins_administrator: %{
+        ClubLive:               [:index, :edit, :show],
+        DepartmentLive:         [:index, :new, :edit, :show],
+        HouseholdLive:          [:index, :new, :edit, :show]
+      },
+
+      vorstand: %{
+        ClubLive:               [:index, :edit, :show],
+        DepartmentLive:         [:index, :show],
+        HouseholdLive:          [:index, :show]
+      }
+    ]
+  end
+
+  def get_roles(atom) when is_atom(atom) do
+    atom
+    |> role_permission_matrix()
+    |> Keyword.keys()
+    |> Enum.map(&(Atom.to_string(&1)))
+    |> Enum.map(&(rename_roles(&1)))
+  end
+
+  defp rename_roles(role) do
+    role
+    |> String.split("_")
+    |> Enum.map(&(String.capitalize(&1)))
+    |> Enum.join(" ")
+  end
+end
