03efb171ac440cfbff74a892507186abf3417d92
Andrew <andrew.utley@studium.fernuni-hagen.de>
2023-02-18 16:05:11 +0100

    ApplicationRole - Fixed tests.

--- test/sportyweb_web/live/application_role_live_test.exs
+++ test/sportyweb_web/live/application_role_live_test.exs
@@ -2,28 +2,44 @@ defmodule SportywebWeb.ApplicationRoleLiveTest do
   use SportywebWeb.ConnCase
 
   import Phoenix.LiveViewTest
+  import Sportyweb.AccountsFixtures
   import Sportyweb.RBAC.RoleFixtures
+  import Sportyweb.RBAC.UserRoleFixtures
 
-  @create_attrs %{name: "some name"}
+  @create_attrs %{name: "some other name"}
   @update_attrs %{name: "some updated name"}
   @invalid_attrs %{name: nil}
 
+  setup do
+    user = user_fixture()
+    applicationrole = application_role_fixture()
+    user_application_role_fixture(%{user_id: user.id, applicationrole_id: applicationrole.id})
+
+    %{user: user}
+  end
+
   defp create_application_role(_) do
-    application_role = application_role_fixture()
+    application_role = application_role_fixture(%{name: "some name"})
     %{application_role: application_role}
   end
 
   describe "Index" do
     setup [:create_application_role]
 
-    test "lists all applicationroles", %{conn: conn, application_role: application_role} do
+    test "lists all applicationroles", %{conn: conn, user: user, application_role: application_role} do
+      {:error, _} = live(conn, ~p"/applicationroles")
+
+      conn = conn |> log_in_user(user)
       {:ok, _index_live, html} = live(conn, ~p"/applicationroles")
 
       assert html =~ "Listing Applicationroles"
       assert html =~ application_role.name
     end
 
-    test "saves new application_role", %{conn: conn} do
+    test "saves new application_role", %{conn: conn, user: user} do
+      {:error, _} = live(conn, ~p"/applicationroles")
+
+      conn = conn |> log_in_user(user)
       {:ok, index_live, _html} = live(conn, ~p"/applicationroles")
 
       assert index_live |> element("a", "New Application role") |> render_click() =~
@@ -45,7 +61,10 @@ defmodule SportywebWeb.ApplicationRoleLiveTest do
       assert html =~ "some name"
     end
 
-    test "updates application_role in listing", %{conn: conn, application_role: application_role} do
+    test "updates application_role in listing", %{conn: conn, user: user, application_role: application_role} do
+      {:error, _} = live(conn, ~p"/applicationroles")
+
+      conn = conn |> log_in_user(user)
       {:ok, index_live, _html} = live(conn, ~p"/applicationroles")
 
       assert index_live |> element("#applicationroles-#{application_role.id} a", "Edit") |> render_click() =~
@@ -67,7 +86,10 @@ defmodule SportywebWeb.ApplicationRoleLiveTest do
       assert html =~ "some updated name"
     end
 
-    test "deletes application_role in listing", %{conn: conn, application_role: application_role} do
+    test "deletes application_role in listing", %{conn: conn, user: user, application_role: application_role} do
+      {:error, _} = live(conn, ~p"/applicationroles")
+
+      conn = conn |> log_in_user(user)
       {:ok, index_live, _html} = live(conn, ~p"/applicationroles")
 
       assert index_live |> element("#applicationroles-#{application_role.id} a", "Delete") |> render_click()
@@ -78,14 +100,20 @@ defmodule SportywebWeb.ApplicationRoleLiveTest do
   describe "Show" do
     setup [:create_application_role]
 
-    test "displays application_role", %{conn: conn, application_role: application_role} do
+    test "displays application_role", %{conn: conn, user: user, application_role: application_role} do
+      {:error, _} = live(conn, ~p"/applicationroles")
+
+      conn = conn |> log_in_user(user)
       {:ok, _show_live, html} = live(conn, ~p"/applicationroles/#{application_role}")
 
       assert html =~ "Show Application role"
       assert html =~ application_role.name
     end
 
-    test "updates application_role within modal", %{conn: conn, application_role: application_role} do
+    test "updates application_role within modal", %{conn: conn, user: user, application_role: application_role} do
+      {:error, _} = live(conn, ~p"/applicationroles")
+
+      conn = conn |> log_in_user(user)
       {:ok, show_live, _html} = live(conn, ~p"/applicationroles/#{application_role}")
 
       assert show_live |> element("a", "Edit") |> render_click() =~
