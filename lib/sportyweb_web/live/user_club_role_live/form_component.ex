d6af0adf29d2bec5ff822fe82a27598d3def57d4
Andrew <andrew.utley@studium.fernuni-hagen.de>
2023-02-19 11:47:43 +0100

    Reset useclubroles

---
+++ lib/sportyweb_web/live/user_club_role_live/form_component.ex
@@ -0,0 +1,81 @@
+defmodule SportywebWeb.UserClubRoleLive.FormComponent do
+  use SportywebWeb, :live_component
+
+  alias Sportyweb.RBAC.UserRole
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div>
+      <.header>
+        <%= @title %>
+        <:subtitle>Use this form to manage user_club_role records in your database.</:subtitle>
+      </.header>
+
+      <.simple_form
+        :let={_f}
+        for={@changeset}
+        id="user_club_role-form"
+        phx-target={@myself}
+        phx-change="validate"
+        phx-submit="save"
+      >
+
+        <:actions>
+          <.button phx-disable-with="Saving...">Save User club role</.button>
+        </:actions>
+      </.simple_form>
+    </div>
+    """
+  end
+
+  @impl true
+  def update(%{user_club_role: user_club_role} = assigns, socket) do
+    changeset = UserRole.change_user_club_role(user_club_role)
+
+    {:ok,
+     socket
+     |> assign(assigns)
+     |> assign(:changeset, changeset)}
+  end
+
+  @impl true
+  def handle_event("validate", %{"user_club_role" => user_club_role_params}, socket) do
+    changeset =
+      socket.assigns.user_club_role
+      |> UserRole.change_user_club_role(user_club_role_params)
+      |> Map.put(:action, :validate)
+
+    {:noreply, assign(socket, :changeset, changeset)}
+  end
+
+  def handle_event("save", %{"user_club_role" => user_club_role_params}, socket) do
+    save_user_club_role(socket, socket.assigns.action, user_club_role_params)
+  end
+
+  defp save_user_club_role(socket, :edit, user_club_role_params) do
+    case UserRole.update_user_club_role(socket.assigns.user_club_role, user_club_role_params) do
+      {:ok, _user_club_role} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "User club role updated successfully")
+         |> push_navigate(to: socket.assigns.navigate)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, :changeset, changeset)}
+    end
+  end
+
+  defp save_user_club_role(socket, :new, user_club_role_params) do
+    case UserRole.create_user_club_role(user_club_role_params) do
+      {:ok, _user_club_role} ->
+        {:noreply,
+         socket
+         |> put_flash(:info, "User club role created successfully")
+         |> push_navigate(to: socket.assigns.navigate)}
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        {:noreply, assign(socket, changeset: changeset)}
+    end
+  end
+end
--- lib/sportyweb_web/live/user_club_role_live/index.ex
+++ lib/sportyweb_web/live/user_club_role_live/index.ex
@@ -1,22 +1,12 @@
 defmodule SportywebWeb.UserClubRoleLive.Index do
   use SportywebWeb, :live_view
 
-  alias Sportyweb.Organization
-
   alias Sportyweb.RBAC.UserRole
   alias Sportyweb.RBAC.UserRole.UserClubRole
 
-  defmodule RoleAdministration do
-    defstruct [:id, :email, :roles]
-  end
-
   @impl true
   def mount(_params, _session, socket) do
-
-    {:ok,
-      socket
-      |> assign(:club_navigation_current_item, :authorization)
-    }
+    {:ok, assign(socket, :userclubroles, list_userclubroles())}
   end
 
   @impl true
@@ -36,14 +26,10 @@ defmodule SportywebWeb.UserClubRoleLive.Index do
     |> assign(:user_club_role, %UserClubRole{})
   end
 
-  defp apply_action(socket, :index, %{"club_id" => club_id} = params) do
-    club = Organization.get_club!(club_id)
-
+  defp apply_action(socket, :index, _params) do
     socket
     |> assign(:page_title, "Listing Userclubroles")
     |> assign(:user_club_role, nil)
-    |> assign(:club, club)
-    |> assign(:userclubroles, list_role_administration(list_userclubroles(params)))
   end
 
   @impl true
@@ -56,29 +42,5 @@ defmodule SportywebWeb.UserClubRoleLive.Index do
 
   defp list_userclubroles do
     UserRole.list_userclubroles()
-  end
-
-  defp list_userclubroles(%{"club_id" => club_id}) do
-    UserRole.list_clubs_userclubroles(club_id)
-  end
-
-  defp list_role_administration(list_of_ucrs) do
-    sort = Sportyweb.RBAC.Role.RolePermissionMatrix.get_role_names(:club)
-
-    list_of_ucrs
-    |> Enum.reduce(%{}, fn x, acc ->
-        Map.merge(acc, %{x.user => [x]}, fn _k, roles, role -> roles ++ role end)
-      end)
-    |> Enum.to_list()
-    |> Enum.map(fn {key, value} ->
-        %RoleAdministration{
-          id: key.id,
-          email: key.email,
-          roles: value
-            |> Enum.map(fn x -> x.clubrole.name end)
-            |> Enum.sort(&(Enum.find_index(sort, fn x -> x == &1 end) <= Enum.find_index(sort, fn x -> x == &2 end)))
-            |> Enum.join(", ")
-        }
-      end)
   end
 end
