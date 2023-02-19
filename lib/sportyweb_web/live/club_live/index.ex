ff992944f552ef3bac5b5a71f6afb61c7189523f
Andrew <andrew.utley@studium.fernuni-hagen.de>
2023-02-19 18:23:46 +0100

    Bug-fixing after Merge

--- lib/sportyweb_web/live/club_live/index.ex
+++ lib/sportyweb_web/live/club_live/index.ex
@@ -1,14 +1,11 @@
 defmodule SportywebWeb.ClubLive.Index do
   use SportywebWeb, :live_view
 
-  alias Sportyweb.AccessControl.PolicyClub
-
   alias Sportyweb.Organization
-  alias Sportyweb.Organization.Club
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :clubs, [])}
+    {:ok, assign(socket, :clubs, list_clubs())}
   end
 
   @impl true
@@ -16,40 +13,13 @@ defmodule SportywebWeb.ClubLive.Index do
     {:noreply, apply_action(socket, socket.assigns.live_action, params)}
   end
 
-  defp apply_action(socket, :edit, %{"id" => id}) do
-    socket
-    |> assign(:page_title, "Edit Club")
-    |> assign(:club, Organization.get_club!(id))
-  end
-
-  defp apply_action(socket, :new, _params) do
-    socket
-    |> assign(:page_title, "New Club")
-    |> assign(:club, %Club{})
-  end
-
   defp apply_action(socket, :index, _params) do
     socket
-    |> assign(:clubs, PolicyClub.load_authorized_clubs(socket.assigns.current_user))
     |> assign(:page_title, "Alle Vereine")
     |> assign(:club, nil)
   end
 
-  @impl true
-  def handle_event("delete", %{"id" => id}, socket) do
-    if PolicyClub.can?(socket.assigns.current_user, "delete", id) do
-      club = Organization.get_club!(id)
-      {:ok, _} = Organization.delete_club(club)
-
-      {:noreply,
-        socket
-        |> put_flash(:info, "Club deleted successfully")
-        |> assign(:clubs, PolicyClub.load_authorized_clubs(socket.assigns.current_user))}
-    else
-      {:noreply,
-        socket
-        |> put_flash(:error, "No permission to delete club")
-        |> assign(:clubs, PolicyClub.load_authorized_clubs(socket.assigns.current_user))}
-    end
+  defp list_clubs do
+    Organization.list_clubs()
   end
 end
