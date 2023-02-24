85584b839c03844a037575f74061ff2d02024a0a
krewast <bastian.kres@krewast.de>
2023-02-24 14:32:56 +0100

    CommonHelper: Added function to generate a comma separated string from a list of structs

--- lib/sportyweb_web/live/common_helper.ex
+++ lib/sportyweb_web/live/common_helper.ex
@@ -15,6 +15,34 @@ defmodule SportywebWeb.CommonHelper do
     end
   end
 
+  @doc """
+  Takes a list of structs and returns a comma separated list of one attribute.
+  If the list is empty, it return a string containg a hyphen.
+
+  ## Examples
+
+      iex> format_struct_list([%Club{name: "FCB"}, %Club{name: "FCN"}], :name)
+      "FCB, FCN"
+
+      iex> format_struct_list([], :name)
+      "-"
+
+  """
+  def format_struct_list(list, attribute) do
+    csv = list
+    |> Enum.filter(fn element ->
+      value = Map.get(element, attribute)
+      !is_nil(value) && value != ""
+    end)
+    |> Enum.map_join(", ", fn element -> Map.get(element, attribute) end)
+
+    if String.trim(csv) != "" do
+      csv
+    else
+      "-"
+    end
+  end
+
   def get_key_for_value(data, value) do
     case Enum.find(data, fn element -> element[:value] == value end) do
       [{:key, key} | _] -> key
