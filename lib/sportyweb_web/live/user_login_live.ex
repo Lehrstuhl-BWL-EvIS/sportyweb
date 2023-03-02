cf2f7e58ddce52d01099588b49d235f40de499f6
krewast <bastian.kres@krewast.de>
2023-03-02 10:53:38 +0100

    Upgrade to Phoenix 1.7 Final (Fixed the deprecation warnings which suddenly popped up due to the generated code in user_login_live.ex. A general fix for this issue seems to be on its way: https://github.com/phoenixframework/phoenix/pull/5310)

--- lib/sportyweb_web/live/user_login_live.ex
+++ lib/sportyweb_web/live/user_login_live.ex
@@ -39,7 +39,7 @@ defmodule SportywebWeb.UserLoginLive do
 
   def mount(_params, _session, socket) do
     email = live_flash(socket.assigns.flash, :email)
-    form = to_form(%{email: email}, as: "user")
+    form = to_form(%{"email" => email}, as: "user")
     {:ok, assign(socket, form: form), temporary_assigns: [form: form]}
   end
 end
