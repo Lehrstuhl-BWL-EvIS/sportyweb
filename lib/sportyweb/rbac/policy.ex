7749a81bef5ac569f349a7410fda34c71e23515c
Andrew <andrew.utley@studium.fernuni-hagen.de>
2023-03-23 23:25:12 +0100

    Major policy and RPM update. Streams, error redirect, button config. Role definition.

--- lib/sportyweb/rbac/policy.ex
+++ lib/sportyweb/rbac/policy.ex
@@ -10,6 +10,7 @@ defmodule Sportyweb.RBAC.Policy do
   alias Sportyweb.RBAC.UserRole
   alias Sportyweb.RBAC.Role.RolePermissionMatrix, as: RPM
 
+  # <--- Policy on_mount hook to enforce authorization based on RBAC concept ---> #
   def on_mount(:permissions, params, _session, socket) do
     action = socket.assigns.live_action
     view = get_live_view(socket.view)
@@ -17,7 +18,10 @@ defmodule Sportyweb.RBAC.Policy do
     if is_application_admin_or_tester(socket.assigns.current_user)
        || permit?(socket.assigns.current_user, action, view, params)
     do
-      {:cont, socket |> add_streams(view, action)}
+      {:cont, socket
+        |> add_streams(view, action)
+        |> add_assigns(view, action)
+      }
     else
       {:halt,
         socket
@@ -26,6 +30,9 @@ defmodule Sportyweb.RBAC.Policy do
     end
   end
 
+  defp get_live_view(view), do: view |> Kernel.inspect() |> String.split(".") |> Enum.at(1) |> String.to_atom()
+
+  # <--- Policy check for application admins ---> #
   def is_application_admin_or_tester(user) do
     user
     |> UserRole.list_users_applicationroles()
@@ -35,6 +42,7 @@ defmodule Sportyweb.RBAC.Policy do
     |> Enum.member?(true)
   end
 
+  # <--- Policy check for club and department role permissions ---> #
   def permit?(_user, :index, :ClubLive, _params), do: true
   def permit?(_user, :new, :ClubLive, _params), do: false
 
@@ -66,13 +74,11 @@ defmodule Sportyweb.RBAC.Policy do
 
   def permit?(user, action, :EquipmentLive = view, params) do
     venue_id = if Map.has_key?(params, "venue_id"), do: params["venue_id"], else: params["id"] |>  get_associated_id(view)
-    permit?(user, action, :VenueLive, %{"id" => venue_id})
+    club_id = venue_id |> Asset.get_venue!() |> Map.get(:club_id)
+    is_allowed?(user.id, action, club_id, view)
   end
 
   def permit?(_user, _action, _view, _params), do: false
-
-
-  defp get_live_view(view), do: view |> Kernel.inspect() |> String.split(".") |> Enum.at(1) |> String.to_atom()
 
   defp is_allowed?(user_id, action, club_id, view, dept_id \\ nil) do
     user_roles = list_users_roles_in_club(user_id, club_id, dept_id)
@@ -123,8 +129,98 @@ defmodule Sportyweb.RBAC.Policy do
     |> Kernel.++(current_roles)
   end
 
-  defp add_streams(socket, :ClubLive, :index), do: Phoenix.LiveView.stream(socket, :clubs, load_associated_clubs(socket.assigns.current_user))
+  defp get_associated_id(id, view) when view in [:ClubLive, :RoleLive], do: id
+  defp get_associated_id(id, view) do
+    case view do
+      :DepartmentLive -> id |> Organization.get_department!() |> Map.get(:club_id)
+      :GroupLive -> id |> Organization.get_group!() |> Map.get(:department_id)
+      :EventLive -> id |> Calendar.get_event!() |> Map.get(:club_id)
+      :ContactLive -> id |> Personal.get_contact!() |> Map.get(:club_id)
+      :VenueLive  -> id |> Asset.get_venue!() |> Map.get(:club_id)
+      :EquipmentLive -> id |> Asset.get_equipment!() |> Map.get(:venue_id)
+      :FeeLive -> id |> Legal.get_fee!() |> Map.get(:club_id)
+    end
+  end
+
+  # <--- Policy controlled redirect ---> #
+
+  defp error_redirect(action, :ClubLive, _params) when action in [:new, :show], do: ~p"/clubs/"
+
+  defp error_redirect(:index, _view, %{"club_id" => club_id}), do: ~p"/clubs/#{club_id}/"
+  defp error_redirect(:index, _view, %{"department_id" => department_id}), do: ~p"/departments/#{department_id}/"
+  defp error_redirect(:index, _view, %{"venue_id" => venue_id}), do: ~p"/venues/#{venue_id}/"
+  defp error_redirect(:edit, view, params), do: get_redirect_path_edit(view, params)
+  defp error_redirect(action, view, params) when action in [:new, :show], do: get_redirect_path_new_show(view, params)
+
+  defp error_redirect(_action, _view, _params), do: ~p"/clubs/"
+
+  defp get_redirect_path_new_show(view, %{"id" => id}) do
+    associated_id = get_associated_id(id, view)
+    case view do
+      :GroupLive -> get_redirect_path_new_show(view, %{"department_id" => associated_id})
+      :EquipmentLive -> get_redirect_path_new_show(view, %{"venue_id" => associated_id})
+      _ -> get_redirect_path_new_show(view, %{"club_id" => associated_id})
+    end
+  end
+
+  defp get_redirect_path_new_show(view, %{"club_id" => club_id}) do
+    case view do
+      :RoleLive -> ~p"/clubs/#{club_id}/roles"
+      :DepartmentLive -> ~p"/clubs/#{club_id}/departments/"
+      :EventLive -> ~p"/clubs/#{club_id}/events/"
+      :ContactLive -> ~p"/clubs/#{club_id}/contacts"
+      :VenueLive -> ~p"/clubs/#{club_id}/venues/"
+      :FeeLive -> ~p"/clubs/#{club_id}/fees/"
+    end
+  end
+  defp get_redirect_path_new_show(:GroupLive, %{"department_id" => department_id}), do: ~p"/departments/#{department_id}/groups/"
+  defp get_redirect_path_new_show(:EquipmentLive, %{"venue_id" => venue_id}), do: ~p"/venues/#{venue_id}/equipment/"
+
+  defp get_redirect_path_edit(view, %{"id" => id}) do
+    case view do
+      :ClubLive -> ~p"/clubs/#{id}/"
+      :DepartmentLive -> ~p"/departments/#{id}/"
+      :GroupLive -> ~p"/groups/#{id}/"
+      :EventLive -> ~p"/events/#{id}/"
+      :ContactLive -> ~p"/contacts/#{id}/"
+      :VenueLive -> ~p"/venues/#{id}/"
+      :EquipmentLive -> ~p"/equipment/#{id}/"
+      :FeeLive -> ~p"/fees/#{id}/"
+    end
+  end
+  defp get_redirect_path_edit(:RoleLive, %{"club_id" => id}), do: ~p"/clubs/#{id}/roles/"
+
+  # <--- Policy controlled buttons ---> #
+  defmodule ButtonConfig, do: defstruct [:id, :index, :edit, :show, :new]
+
+  defp get_button_config(socket, params) do
+    case is_application_admin_or_tester(socket.assigns.current_user) do
+      true -> %ButtonConfig{id: 1, index: true, edit: true, show: true, new: true}
+      _ -> get_button_config(socket.assigns.current_user, get_live_view(socket.view), params)
+    end
+  end
+  defp get_button_config(user, :ClubLive = view, %{} = params) do
+    [index, new] = [:index, :new] |> Enum.map(&(permit?(user, &1, view, params)))
+    %ButtonConfig{id: 1, index: index, edit: false, show: false, new: new}
+  end
+  defp get_button_config(user, view, params) do
+    [index, edit, show, new] = [:index, :edit, :show, :new] |> Enum.map(&(permit?(user, &1, view, params)))
+    %ButtonConfig{id: 1, index: index, edit: edit, show: show, new: new}
+  end
+
+  # <--- Policy streams and assigns ---> #
+  defp add_streams(socket, :ClubLive, :index), do: Phoenix.LiveView.attach_hook(socket, :add_stream_club, :handle_params, &add_stream_club/3)
   defp add_streams(socket, _view, _action), do: socket
+
+  defp add_assigns(socket, _view, _action), do: Phoenix.LiveView.attach_hook(socket, :add_assign_button_config, :handle_params, &add_assign_button_config/3)
+
+  defp add_stream_club(_params, _url, socket) do
+    socket = socket.assigns.current_user
+      |> load_associated_clubs()
+      |> Enum.reduce(socket, &(Phoenix.LiveView.stream_insert(&2, :clubs, &1)))
+
+    {:cont, socket}
+  end
 
   defp load_associated_clubs(user) do
       case is_application_admin_or_tester(user) do
@@ -143,49 +239,7 @@ defmodule Sportyweb.RBAC.Policy do
     |> Kernel.>(0)
   end
 
-  defp error_redirect(action, :ClubLive, _params) when action in [:new, :show], do: ~p"/clubs"
-
-  defp error_redirect(:index, _view, %{"club_id" => club_id}), do: ~p"/clubs/#{club_id}"
-  defp error_redirect(:index, _view, %{"department_id" => department_id}), do: ~p"/departments/#{department_id}"
-  defp error_redirect(:index, _view, %{"venue_id" => venue_id}), do: ~p"/venues/#{venue_id}"
-
-  defp error_redirect(:new, view, %{"club_id" => club_id}), do: ~p"/clubs/#{club_id}/#{get_related_path(view)}"
-  defp error_redirect(:new, view, %{"department_id" => department_id}), do: ~p"/departments/#{department_id}/#{get_related_path(view)}"
-  defp error_redirect(:new, view, %{"venue_id" => venue_id}), do: ~p"/venues/#{venue_id}/#{get_related_path(view)}"
-
-  defp error_redirect(:edit, view, %{"id" => id}), do: ~p"/#{get_related_path(view)}/#{id}"
-
-  defp error_redirect(:show, :GroupLive = view, %{"id" => id}), do: ~p"/departments/#{get_associated_id(id, view)}/#{get_related_path(view)}"
-  defp error_redirect(:show, :EquipmentLive = view, %{"id" => id}), do: ~p"/venues/#{get_associated_id(id, view)}/#{get_related_path(view)}"
-  defp error_redirect(:show, view, %{"id" => id}), do: ~p"/clubs/#{get_associated_id(id, view)}/#{get_related_path(view)}"
-
-  defp error_redirect(_action, _view, _params), do: ~p"/clubs"
-
-  defp get_related_path(view) do
-    case view do
-      :ClubLive -> "clubs"
-      :RoleLive -> "roles"
-      :DepartmentLive -> "departments"
-      :GroupLive -> "groups"
-      :EventLive -> "events"
-      :ContactLive -> "contacts"
-      :VenueLive -> "venues"
-      :EquipmentLive -> "equipment"
-      :FeeLive -> "fees"
-    end
-  end
-
-  defp get_associated_id(id, view) do
-    case view do
-      :ClubLive -> id
-      :RoleLive -> id
-      :DepartmentLive -> id |> Organization.get_department!() |> Map.get(:club_id)
-      :GroupLive -> id |> Organization.get_group!() |> Map.get(:department_id)
-      :EventLive -> id |> Calendar.get_event!() |> Map.get(:club_id)
-      :ContactLive -> id |> Personal.get_contact!() |> Map.get(:club_id)
-      :VenueLive  -> id |> Asset.get_venue!() |> Map.get(:club_id)
-      :EquipmentLive -> id |> Asset.get_equipment!() |> Map.get(:venue_id)
-      :FeeLive -> id |> Legal.get_fee!() |> Map.get(:club_id)
-    end
+  defp add_assign_button_config(params, _url, socket) do
+    {:cont, socket |> Phoenix.Component.assign(:button_config, get_button_config(socket, params))}
   end
 end
--- lib/sportyweb/rbac/role/role_permission_matrix.ex
+++ lib/sportyweb/rbac/role/role_permission_matrix.ex
@@ -16,9 +16,9 @@ defmodule Sportyweb.RBAC.Role.RolePermissionMatrix do
 
   def role_permission_matrix(:club) do
     [
-      club_admin: %{
-        Name:                   "Vereins Administration",
-        Info:                   "verfügt über alle Zugriffsrechte im Kontext des zugrunde liegenden Vereins.",
+      verein_admin: %{
+        Name:                   "Vereinsadministration",
+        Info:                   "verfügt den vollen lesenden und schreibenden Zugriff auf alle Ressourcen und Operationen im jeweiligen Verein.",
         ClubLive:               [:edit],
         RoleLive:               [:index, :new, :edit, :show],
         EventLive:              [:index, :new, :edit, :show],
@@ -30,57 +30,67 @@ defmodule Sportyweb.RBAC.Role.RolePermissionMatrix do
         FeeLive:                [:index, :new, :edit, :show],
       },
 
-      vorstand: %{
+      verein_lead: %{
         Name:                   "Geschäftsführung",
+        Info:                   "verfügt über vollen lesenden und schreibenden Zugriff auf alle Ressourcen und Operationen, die im Kontext der Vereinsführung stehen. Dabei wird primär auf die Themenbereiche Vereinsverwaltung, Finanzen, Kommunikation und Sponsoring abgestellt. Für alle anderen Inhalte sind lediglich Leserechte gewährt.",
         ClubLive:               [:edit],
-        DepartmentLive:         [:show],
-        HouseholdLive:          [:index, :show],
-        RoleLive:               [:index, :show],
-        Info:                   "verfügt über die notwendigen Berechtigungen, sich die Inhalte aller Bereiche anzeigen zu lassen und die des Vereins zudem zu editieren."
+        RoleLive:               [:index, :new, :edit, :show],
+        EventLive:              [:index, :show],
+        DepartmentLive:         [:index, :new, :show],
+        GroupLive:              [:index, :new, :show],
+        ContactLive:            [:index, :new, :edit, :show],
+        VenueLive:              [:index, :show],
+        EquipmentLive:          [:index, :show],
+        FeeLive:                [:index, :new, :edit, :show],
       },
 
       rollen_verwaltung: %{
         Name:                   "Rollenverwaltung",
+        Info:                   "verfügt über die notwendigen Zugriffe, um Nutzern im Verein Rollen zuweisen und entziehen zu können.",
         RoleLive:               [:index, :new, :edit, :show],
-        Info:                   "verfügt über die Berechtigungen, sich die Rollen der Nutzer eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
-
       },
 
       vereins_mitglied: %{
         Name:                   "Vereinsmitglied",
+        Info:                   "verügt über die notwendigen Zugriffe Vereins- und Abteilungsinformationen und -angebote einzusehen, die sich an alle Mitglieder richten.",
+        EventLive:              [:index, :show],
         DepartmentLive:         [:show],
-        Info:                   "verfügt über die Berechtigungen, sich Vereins- und Abteilungsinformationen und -angebote einzusehen, die sich an alle Mitglieder richten."
+        GroupLive:              [:index],
       },
 
       finanz_verwaltung: %{
         Name:                   "Finanzverwaltung",
-        Info:                   "verfügt über die Berechtigungen, sich die Finanzen eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen, die im Kontext der Finanzverwaltung stehen.",
+        FeeLive:                [:index, :new, :edit, :show],
       },
 
       schiedsstelle: %{
         Name:                   "Schiedsstelle",
-        Info:                   "verfügt über die Berechtigungen, sich die Mitglieder eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen, die im Kontext der Bearbeitung von Streitfällen stehen.",
       },
 
       pr: %{
         Name:                   "Öffentlichkeitsarbeit",
-        Info:                   "verfügt über die Berechtigungen, sich die relevanten Inhalte im Kontext der Öffentlichkeitsarbeit eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen, die im Kontext der Öffentlichkeitsarbeit stehen.",
+        ContactLive:            [:index, :new, :edit, :show],
       },
 
       jugendleitung: %{
         Name:                   "Jugendleitung",
-        Info:                   "verfügt über die Berechtigungen, sich die Mitglieder eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen, die im Kontext der Arbeit mit Jugendlichen stehen.",
       },
 
       anlagen_geraete_verwaltung: %{
         Name:                   "Anlagen- & Geräteverwaltung",
-        Info:                   "verfügt über die Berechtigungen, sich die Anlagen und Geräte eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen, die im Kontext der Verwaltung von Anlagen und Geräten des Vereins stehen.",
+        VenueLive:              [:index, :new, :edit, :show],
+        EquipmentLive:          [:index, :new, :edit, :show],
       },
 
       mitglieder_verwaltung: %{
         Name:                   "Mitgliederverwaltung",
-        HouseholdLive:          [:index, :new, :edit, :show],
-        Info:                   "verfügt über die Berechtigungen, sich die Mitglieder eines Vereins anzeigen zu lassen sowie diese zu bearbeiten."
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen, die im Kontext der Verwaltung von Mitgliedern stehen.",
+        ContactLive:            [:index, :new, :edit, :show],
       },
     ]
   end
@@ -89,34 +99,32 @@ defmodule Sportyweb.RBAC.Role.RolePermissionMatrix do
     [
       abteilungsleiter: %{
         Name:                   "Abteilungsleiter",
-        Info:                   "verfügt über die Berechtigungen, sich eine Abteilung eines Vereins anzeigen zu lassen sowie diese zu bearbeiten.",
+        Info:                   "verfügt über -	die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen innerhalb einer Abteilung.",
         DepartmentLive:         [:edit],
         GroupLive:              [:index, :new, :edit, :show],
-        VenueLive:              [:index, :new, :edit, :show],
       },
 
       trainer: %{
         Name:                   "Trainer",
-        Info:                   "verfügt über die Berechtigungen, sich eine Abteilung eines Vereins anzeigen zu lassen sowie trainingsrelevante Inhalte darin zu bearbeiten.",
-        VenueLive:              [:index, :new, :edit, :show],
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen innerhalb der dedizierten Abteilung, die im Kontext der Trainings- und Übungsleitung stehen.",
+        GroupLive:              [:index, :edit, :show],
       },
 
       wettampfverwaltung: %{
         Name:                   "Wettkampfverwaltung",
-        Info:                   "verfügt über die Berechtigungen, sich eine Abteilung eines Vereins anzeigen zu lassen sowie wettkampfsrelevante Inhalte darin zu bearbeiten.",
-        VenueLive:              [:index, :new, :edit, :show],
+        Info:                   "verfügt über die notwendigen lesenden und schreibenden Zugriffe auf alle Ressourcen und Operationen innerhalb der dedizierten Abteilung, die im Kontext des Wettkampf- und Ligabetriebs stehen.",
+        GroupLive:              [:index, :edit, :show],
       },
 
       abteilungsmitglied: %{
         Name:                   "Abteilungsmitglied",
-        Info:                   "verfügt über die Berechtigungen, sich eine Abteilung eines Vereins anzeigen zu lassen.",
-        VenueLive:              [:index, :new, :edit, :show],
+        Info:                   "verfügt über die notwendigen Zugriffe Abteilungsinformationen und -angebote einzusehen, die sich nur an Abteilungsmitglieder richten.",
+        GroupLive:              [:index, :show],
       },
 
       sponsor: %{
         Name:                   "Sponsor",
-        Info:                   "verfügt über die Berechtigungen, sich den Sponsorenbereich einer Abteilung eines Vereins anzeigen zu lassen.",
-        VenueLive:              [:index, :new, :edit, :show],
+        Info:                   "verfügt über die notwendigen Zugriffe Abteilungsinformationen und -angebote einzusehen, die sich an ausgewählte Dritte des Vereins richten.",
       }
     ]
   end
