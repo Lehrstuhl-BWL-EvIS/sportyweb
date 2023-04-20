daef83634f501c47929ffd7f8af51f737f05f500
Andrew <andrew.utley@studium.fernuni-hagen.de>
2023-04-20 18:39:45 +0200

    Fixed bug in user confirmation instrutions live.

--- lib/sportyweb_web/live/user_confirmation_instructions_live.ex
+++ lib/sportyweb_web/live/user_confirmation_instructions_live.ex
@@ -28,12 +28,14 @@ defmodule SportywebWeb.UserConfirmationInstructionsLive do
   end
 
   def handle_event("send_instructions", %{"user" => %{"email" => email}}, socket) do
-    if user = Accounts.get_user_by_email(email) do
+    user = Accounts.get_user_by_email(email)
+    confirmation = if user, do: user.confirmed_at, else: nil
+    if user && confirmation == nil do
       Accounts.get_user_by_email("timing_attack_dummy@sportyweb.de")
-      Accounts.deliver_user_confirmation_instructions(user, &url(~p"/users/reset_password/#{&1}"))
+      Accounts.deliver_user_confirmation_instructions(user, &url(~p"/users/confirm/#{&1}"))
     else
       dummy = Accounts.get_user_by_email("timing_attack_dummy@sportyweb.de")
-      Accounts.deliver_user_confirmation_instructions(dummy, &url(~p"/users/reset_password/#{&1}"))
+      Accounts.deliver_user_confirmation_instructions(dummy, &url(~p"/users/confirm/#{&1}"))
     end
 
     info =
