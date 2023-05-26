3e1d56eceac7de605ef9d2b932b54e5d97dbaadc
krewast <bastian.kres@krewast.de>
2023-05-26 20:07:25 +0200

    Note: Added reusable LiveComponent to show each instance of this entity.

---
+++ lib/sportyweb_web/live/polymorphic_live/notes_show_component.ex
@@ -0,0 +1,24 @@
+defmodule SportywebWeb.PolymorphicLive.NotesShowComponent do
+  use SportywebWeb, :live_component
+
+  import SportywebWeb.CommonHelper
+
+  @impl true
+  def render(assigns) do
+    ~H"""
+    <div>
+      <%= if Enum.any?(@notes) do %>
+        <div class="divide-y divide-zinc-100">
+          <%= for note <- @notes do %>
+            <div class="py-4 first:pt-0 last:pb-0">
+              <%= format_string_field(note.content) %>
+            </div>
+          <% end %>
+        </div>
+      <% else %>
+        -
+      <% end %>
+    </div>
+    """
+  end
+end
