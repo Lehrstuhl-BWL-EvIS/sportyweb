cd21deb519858a2452c2b1c07825594990b81361
krewast <bastian.kres@krewast.de>
2023-06-21 07:27:02 +0200

    Forecasts: Don't allow "forecasts" for past dates.

--- lib/sportyweb/finance/forecast.ex
+++ lib/sportyweb/finance/forecast.ex
@@ -7,6 +7,7 @@ defmodule Sportyweb.Finance.Forecast do
     field :type, :string, default: "contact", virtual: true
     field :contact_id, :string, default: "", virtual: true
     field :subsidy_id, :string, default: "", virtual: true
+    field :current_date, :date, default: Date.utc_today(), virtual: true
     field :start_date, :date, default: nil, virtual: true
     field :end_date, :date, default: nil, virtual: true
   end
@@ -26,6 +27,8 @@ defmodule Sportyweb.Finance.Forecast do
       empty_values: ["", nil]
     )
     |> validate_required([:type, :start_date, :end_date])
+    |> validate_dates_order(:current_date, :start_date,
+      "Startzeitpunkt darf nicht in der Vergangenheit liegen!")
     |> validate_dates_order(:start_date, :end_date,
       "Muss zeitlich später als oder gleich \"Von\" sein!")
   end
