9a34a862820de64713462e69f3c9789ccb5e129d
krewast <bastian.kres@krewast.de>
2024-04-08 14:35:38 +0200

    Config: Changed the name of the development database

--- config/dev.exs
+++ config/dev.exs
@@ -5,7 +5,7 @@ config :sportyweb, Sportyweb.Repo,
   username: "postgres",
   password: "postgres",
   hostname: "localhost",
-  database: "sportyweb17_dev",
+  database: "sportyweb_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
