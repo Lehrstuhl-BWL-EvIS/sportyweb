e59946185d1457c539e421c7c2eade48e529dd5a
Casey Gielen <cagie@arcor.de>
2025-12-30 20:25:50 +0100

    Mix format

--- priv/repo/migrations/20230605122539_create_transactions.exs
+++ priv/repo/migrations/20230605122539_create_transactions.exs
@@ -25,6 +25,9 @@ defmodule Sportyweb.Repo.Migrations.CreateTransactions do
     create index(:transactions, [:contract_id])
     create index(:transactions, [:contact_id])
     create index(:transactions, [:club_id])
-    create unique_index(:transactions, [:receipt_number, :club_id], name: :unique_receipt_number_club_index)
+
+    create unique_index(:transactions, [:receipt_number, :club_id],
+             name: :unique_receipt_number_club_index
+           )
   end
 end
